1c9a79e42fa1ccb2d01be38028168bf4f7e673ff
rares8283682 <rares2002olt@yahoo.com>
2026-06-05 10:52:16 +0200

    Add create_powerset to module exports

--- src/socialranking/__init__.py
+++ src/socialranking/__init__.py
@@ -1,9 +1,10 @@
 """Social ranking tools."""
-
+from socialranking.helpers import create_powerset
 from socialranking.power_relation import PowerRelation
 from socialranking.social_ranking import SocialRanking
 
 __all__ = [
     "PowerRelation",
     "SocialRanking",
+    "create_powerset",
 ]
