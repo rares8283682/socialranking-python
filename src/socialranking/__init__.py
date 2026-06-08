0d24b7273b70a74a35631cca77002a8b156741d4
rares8283682 <rares2002olt@yahoo.com>
2026-06-08 18:13:30 +0200

    new

--- src/socialranking/__init__.py
+++ src/socialranking/__init__.py
@@ -2,9 +2,12 @@
 from socialranking.helpers import create_powerset
 from socialranking.power_relation import PowerRelation
 from socialranking.social_ranking import SocialRanking
+from socialranking.rankings import lexcel_scores, lexcel_ranking
 
 __all__ = [
     "PowerRelation",
     "SocialRanking",
     "create_powerset",
+    "lexcel_scores",
+    "lexcel_ranking",
 ]
